3ba2633c64c4755b9f7c78dc9267268b3a46376e
Botu Lyu <avgstuBoboge@gmail.com>
2025-04-18 15:48:20 +0200

    feat: more sketch; fix: page title.

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,23 +1,34 @@
 <script setup>
-
 </script>
 
 <template>
   <div class="nav-container">
-    <div class="logo-container">
-      <el-image
-          style="height: 100px;"
-          src="swiss-flag.png"
-          fit="contain"/>
+    <div class="head-container">
+      <div class="logo-container">
+        <el-image
+            style="height: 100px;"
+            src="swiss-flag.png"
+            fit="contain"/>
+      </div>
+      <h1>Happiness Index Across Swiss Cantons</h1>
     </div>
-    <h1>Swiss Map Demo</h1>
+    <el-menu mode="horizontal" default-active="1">
+      <el-menu-item index="1" style="font-size: 20px">Overview</el-menu-item>
+      <el-menu-item index="2" style="font-size: 20px">Ranking</el-menu-item>
+    </el-menu>
   </div>
 </template>
 
 <style scoped>
 .nav-container {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+}
+
+.head-container {
+  display: flex;
   align-items: center;
+  justify-content: flex-start;
 }
 </style>
--- src/components/SideBarRight.vue
+++ src/components/SideBarRight.vue
@@ -1,9 +1,17 @@
 <script setup>
+import {ref} from "vue";
 
+const comparing = ref(false);
 </script>
 
 <template>
   <el-card>
+    <el-switch
+        size="large"
+        v-model="comparing"
+        active-text="Compare Cantons"
+        inactive-text="Single Canton"
+    />
     <h3>Region Information</h3>
     <p>Details about the selected region will be shown here.</p>
   </el-card>
